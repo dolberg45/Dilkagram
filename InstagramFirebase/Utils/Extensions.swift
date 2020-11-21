15ac8801d586ff3723891b2c2f5e80da3cd95c16
dolberg45 <dolberg4500@gmail.com>
2020-11-21 18:09:16 +0300

    Перенос расширения  anchor в файл с расширениями.

--- InstagramFirebase/Utils/Extensions.swift
+++ InstagramFirebase/Utils/Extensions.swift
@@ -15,3 +15,47 @@ extension UIColor {
     }
     
 }
+
+extension UIView {
+    /*
+     top - верхняя часть
+     bottom - нижняя часть
+     left - левая часть
+     right - правая часть
+     paddingTop - отступ сверху
+     paddingLeft - отступ слева
+     paddingBottom - отступ снизу
+     paddingRight - отступ справа
+     width - ширина
+     height - высота
+     anchors - опциональны
+     */
+    func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+           self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: paddingRight).isActive = true
+        }
+        
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+}
