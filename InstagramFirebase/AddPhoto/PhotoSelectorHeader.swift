b2fb8b1552aa00c17c994fa244e61532de4acb6e
dolberg45 <dolberg4500@gmail.com>
2020-11-21 16:43:48 +0300

    Добавлена кастомная header ячейка.

---
+++ InstagramFirebase/AddPhoto/PhotoSelectorHeader.swift
@@ -0,0 +1,32 @@
+//
+//  PhotoSelectorHeader.swift
+//  InstagramFirebase
+//
+//  Created by Григорий on 21.11.2020.
+//  Copyright © 2020 Grigoriy Alekseev. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorHeader: UICollectionViewCell {
+
+    let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .cyan
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        photoImageView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
