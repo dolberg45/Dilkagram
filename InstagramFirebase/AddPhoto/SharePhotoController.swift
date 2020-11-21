decd00258cd2a357439904ab04d719aae6bc8bad
dolberg45 <dolberg4500@gmail.com>
2020-11-21 17:23:09 +0300

    Создан контроллер для работы с выбранной фото.

---
+++ InstagramFirebase/AddPhoto/SharePhotoController.swift
@@ -0,0 +1,30 @@
+//
+//  SharePhotoController.swift
+//  InstagramFirebase
+//
+//  Created by Григорий on 21.11.2020.
+//  Copyright © 2020 Grigoriy Alekseev. All rights reserved.
+//
+
+import UIKit
+
+class SharePhotoController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
